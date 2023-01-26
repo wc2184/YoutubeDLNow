6e2fd055df409dcc107e3ef6c974be144dc2fe50
William Chan <wc2184@nyu.edu>
2023-01-26 18:45:52 -0500

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -257,7 +257,7 @@ const Home = () => {
         //   res.headers.get('content-disposition').split('"')[1]
         // } is downloading. Please wait one moment.`,
         status: 'info',
-        duration: 2000,
+        duration: 6000,
         isClosable: true,
       });
     }
